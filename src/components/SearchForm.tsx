c51ab80a0c32df375879e9173cae07c05ce79f88
unknown <jocelynhuang193@outlook.com>
2021-10-02 22:17:22 +1000

    remove slider

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -14,45 +14,6 @@ type SearchFormProps = {
   userId: string;
 };
 
-const PrettoSlider = styled(Slider)({
-  color: '#5c6ac4',
-  height: 8,
-  '& .MuiSlider-track': {
-    border: 'none',
-  },
-  '& .MuiSlider-thumb': {
-    height: 24,
-    width: 24,
-    backgroundColor: '#fff',
-    border: '2px solid currentColor',
-    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-      boxShadow: 'inherit',
-    },
-    '&:before': {
-      display: 'none',
-    },
-  },
-  '& .MuiSlider-valueLabel': {
-    lineHeight: 1.2,
-    fontSize: 12,
-    background: 'unset',
-    padding: 0,
-    width: 32,
-    height: 32,
-    borderRadius: '50% 50% 50% 0',
-    backgroundColor: '#5c6ac4',
-    transformOrigin: 'bottom left',
-    transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-    '&:before': { display: 'none' },
-    '&.MuiSlider-valueLabelOpen': {
-      transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-    },
-    '& > *': {
-      transform: 'rotate(45deg)',
-    },
-  },
-});
-
 export default function SearchForm(props: SearchFormProps) {
   const [
     checkProductPriceByUrl,
@@ -126,6 +87,7 @@ export default function SearchForm(props: SearchFormProps) {
         </form>
         {checkPriceQueryData && (
           <SearchResult
+            userid={props.userId}
             product_image_src={
               checkPriceQueryData?.checkProductPriceByUrl?.product_image_src
             }
@@ -137,72 +99,6 @@ export default function SearchForm(props: SearchFormProps) {
             }
           />
         )}
-        {/* {checkPriceQueryData && (
-          <div className='flex w-full align-middle justify-center mt-10 space-x-10'>
-            <div>
-              <Image
-                src={
-                  checkPriceQueryData?.checkProductPriceByUrl?.product_image_src
-                }
-                width={150}
-                height={200}
-              />
-            </div>
-            <div className='w-full grid grid-cols-4 gap-4'>
-              <div className='col-span-4'>
-                <p className='text-2xl'>
-                  {checkPriceQueryData?.checkProductPriceByUrl?.product_name}
-                </p>
-              </div>
-              <div>
-                <p>
-                  <span className='font-bold'>Price:&nbsp;</span>
-                </p>
-              </div>
-
-              <div className='col-span-3'>
-                <span>
-                  {checkPriceQueryData?.checkProductPriceByUrl?.original_price}
-                </span>
-              </div>
-
-              <div>
-                {' '}
-                <label className='font-bold'>Desired Price:&nbsp;</label>
-              </div>
-              <div className='col-span-3'>
-                <input
-                  className='w-full border-darkgrey border-2 rounded-lg  hover:border-indigo focus:bg-transparent'
-                  type='number'
-                />
-              </div>
-
-              <div>
-                <label className='font-bold'>Desired Discount:&nbsp;</label>
-              </div>
-              <div className='col-span-3'>
-                <PrettoSlider
-                  valueLabelDisplay='auto'
-                  aria-label='pretto slider'
-                  //getAriaValueText={valuetext}
-                  defaultValue={0}
-                  step={0.01}
-                  min={0}
-                  max={1}
-                />
-              </div>
-              <div className='col-span-4'>
-                {' '}
-                <button
-                  className='w-full text-center h-10 rounded-md bg-indigo-400 text-grey hover:bg-indigo disabled:opacity-50 text-lg font-bold'
-                  type='submit'
-                >
-                  Add to Track List
-                </button>
-              </div>
-            </div>
-          </div>
-        )} */}
       </div>
     </>
   );
--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,53 +1,33 @@
 import Slider from '@mui/material/Slider';
 import { styled } from '@mui/material/styles';
 import Image from 'next/dist/client/image';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 type SearchResultProps = {
+  userid: string;
   product_image_src: string;
   product_name: string;
   original_price: number;
 };
 
-const PrettoSlider = styled(Slider)({
-  color: '#5c6ac4',
-  height: 8,
-  '& .MuiSlider-track': {
-    border: 'none',
-  },
-  '& .MuiSlider-thumb': {
-    height: 24,
-    width: 24,
-    backgroundColor: '#fff',
-    border: '2px solid currentColor',
-    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-      boxShadow: 'inherit',
-    },
-    '&:before': {
-      display: 'none',
-    },
-  },
-  '& .MuiSlider-valueLabel': {
-    lineHeight: 1.2,
-    fontSize: 12,
-    background: 'unset',
-    padding: 0,
-    width: 32,
-    height: 32,
-    borderRadius: '50% 50% 50% 0',
-    backgroundColor: '#5c6ac4',
-    transformOrigin: 'bottom left',
-    transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-    '&:before': { display: 'none' },
-    '&.MuiSlider-valueLabelOpen': {
-      transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-    },
-    '& > *': {
-      transform: 'rotate(45deg)',
-    },
-  },
-});
+type AddToTrackListData = {
+  desired_price: number;
+};
 
 export function SearchResult(props: SearchResultProps) {
+  const {
+    register,
+    formState: { errors, isValid, isDirty },
+    handleSubmit,
+  } = useForm<AddToTrackListData>({ mode: 'onChange' });
+
+  const onSubmit: SubmitHandler<AddToTrackListData> = async (formData) => {
+    await {
+      variables: {
+        url: formData.desired_price,
+      },
+    };
+  };
   return (
     <>
       <div className='flex w-full align-middle justify-center mt-10 space-x-10'>
@@ -74,39 +54,34 @@ export function SearchResult(props: SearchResultProps) {
           </div>
 
           <div>
-            {' '}
             <label className='font-bold'>Desired Price:&nbsp;</label>
           </div>
-          <div className='col-span-3'>
-            <input
-              className='w-full border-darkgrey border-2 rounded-lg  hover:border-indigo focus:bg-transparent'
-              type='number'
-            />
-          </div>
-
-          <div>
-            <label className='font-bold'>Desired Discount:&nbsp;</label>
-          </div>
-          <div className='col-span-3'>
-            <PrettoSlider
-              valueLabelDisplay='auto'
-              aria-label='pretto slider'
-              //getAriaValueText={valuetext}
-              defaultValue={0}
-              step={0.01}
-              min={0}
-              max={1}
-            />
-          </div>
-          <div className='col-span-4'>
-            {' '}
-            <button
-              className='w-full text-center h-10 rounded-md bg-indigo-400 text-grey hover:bg-indigo disabled:opacity-50 text-lg font-bold'
-              type='submit'
-            >
-              Add to Track List
-            </button>
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='col-span-3'>
+              <input
+                className='w-full border-darkgrey border-2 rounded-lg  hover:border-indigo focus:bg-transparent'
+                type='number'
+                placeholder='Desired Price'
+                {...register('desired_price', {
+                  maxLength: {
+                    value: 4,
+                    message: 'Maximum length is 4 digits.',
+                  },
+                })}
+              />
+            </div>
+            {errors.desired_price && (
+              <p className='text-red'>{errors.desired_price.message}</p>
+            )}
+            <div className='mt-4'>
+              <button
+                className='w-full text-center h-10 rounded-md bg-indigo-400 text-grey hover:bg-indigo disabled:opacity-50 text-lg font-bold'
+                type='submit'
+              >
+                Add to Track List
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
